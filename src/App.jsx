dccf39ea28c08a8a1d98dbdc4797115c624b9201
ShogoKawamura <101000638+ShogoKawamura@users.noreply.github.com>
2022-06-21 06:03:24 +0900

    Todoの追加機能の実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
+  const [incomleteTodos, setIncompleteTodos] = useState([
+    "ああああ",
+    "いいいい"
+  ]);
+  const [completeTodos, setCompleteTodos] = useState(["うううう"]);
+
+  const onChangeTodoText = (event) => event.target.value;
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incomleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
   return (
     <>
       <div>
-        <input placeholder="Todoを入力" />
-        <button>追加</button>
+        <input
+          placeholder="Todoを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div>
         <p>未完了</p>
         <ul>
-          <div>
-            <li>ああああ</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {incomleteTodos.map((todo) => {
+            return (
+              <div key={todo}>
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+
           <div>
             <li>いいいい</li>
             <button>完了</button>
@@ -26,10 +51,14 @@ export const App = () => {
       <div>
         <p>未完了</p>
         <ul>
-          <div>
-            <li>うううう</li>
-            <button>戻す</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo}>
+                <li>うううう</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
